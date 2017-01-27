3bc3b3b573dc8ab20be8583f62915bb741bc3b10
Pratyush Pratik <pratyushpratik@pratyushs-macbook-air.local>
2017-01-27 20:15:42 +0530

    Initial Commit

---
+++ employee_database_swift/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  employee_database_swift
+//
+//  Created by Pratyush Pratik on 27/01/17.
+//  Copyright © 2017 Pratyush Pratik. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
